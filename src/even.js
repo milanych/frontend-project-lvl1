86940a9b23b9e544984cfe80b06cae3bd0eaddeb
milanych <vadimzotov1988@gmail.com>
2021-03-11 22:45:29 +0300

    Update even.js

--- src/even.js
+++ src/even.js
@@ -2,30 +2,32 @@ import readlineSync from 'readline-sync';
 
 const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ')
 console.log(`Hello, ${name}\nAnwser "yes" if number is even, otherwise answer "no".`);
-let num = Math.floor(Math.random() * 100);
-const question = readlineSync.question('Question: ' + num);
-readlineSync.setDefaultOptions({limit: ['yes', 'no']});
-const answer = readlineSync.question('Your answer: ');
-const isEven = (result) => {
-  if (num % 2 === 0 && answer === 'yes') {
-    return 'Correct!';
-  } else {
-    return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
-  };
-};
-const answer2 = readlineSync.question('Your answer: ');
-const isEven2 = (result) => {
-  if (num % 2 === 0 && answer2 === 'yes') {
-    return 'Correct!';
-  } else {
-    return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
-  };
-};
-const answer3 = readlineSync.question('Your answer: ');
-const isEven3 = (result) => {
-  if (num % 2 === 0 && answer3 === 'yes') {
-    return 'Correct!';
-  } else {
-    return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
-  };
-};
+const random = () => {
+  let result = [];
+  for (let x = 0; x < 3; x++) {
+    result.push(Math.floor(Math.random() * 100));
+  }
+  return result;
+}
+let rand = random();
+const game = () => {
+  for (let i = 0; i < rand.length;) {
+    const isEven = () => {
+      if (rand[i] % 2 === 0) {
+        return 'yes';
+      } 
+      return 'no';
+    };
+   console.log('Question: ' + rand[i]);
+   let answer = readlineSync.question('Your answer: ');
+    if (answer === isEven()) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven()}'.\nLet's try again, ${name}!`)
+      break;
+    }
+      console.log(`Congratulations, ${name}!`);
+  }
+}
+game();
