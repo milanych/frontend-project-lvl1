6969db1398e2493d6d0e4cfb1ce562ec9993731f
milanych <vadimzotov1988@gmail.com>
2021-03-11 22:49:00 +0300

    Update brian-even.js

--- bin/brian-even.js
+++ bin/brian-even.js
@@ -1,2 +1,7 @@
 #!/usr/bin node
+import { name } from '../src/even.js';
+console.log(`Hello, ${name}\nAnwser "yes" if number is even, otherwise answer "no".`);
+import { random } from '../src/even.js';
+let rand = random();
 import { game } from '../src/even.js';
+game();
