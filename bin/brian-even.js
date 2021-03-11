85dbbf4418b75cd9a855a31543a0663e43bdf574
milanych <vadimzotov1988@gmail.com>
2021-03-11 22:29:41 +0300

    Update brian-even.js

--- bin/brian-even.js
+++ bin/brian-even.js
@@ -14,10 +14,10 @@ const game = () => {
   for (let i = 0; i < rand.length;) {
    console.log('Question: ' + rand[i]);
    let answer = readlineSync.question('Your answer: ');
-    if (answer === 'yes' && i % 2 === 0) {
+    if (answer === 'yes' && rand[i] % 2 === 0) {
       console.log('Correct!');
       i += 1;
-    } else if (answer === 'no' && i % 2 !== 0) {
+    } else if (answer === 'no' && rand[i] % 2 !== 0) {
       console.log('Correct!');
       i += 1;
     }
