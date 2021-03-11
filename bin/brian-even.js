a8346fe969a1fc87628e13380f8cb7840671960c
milanych <vadimzotov1988@gmail.com>
2021-03-11 21:55:40 +0300

    Update brian-even.js

--- bin/brian-even.js
+++ bin/brian-even.js
@@ -1,5 +1,34 @@
 #!/usr/bin node
-import { name } from '../src/even.js';
+const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ')
+console.log(`Hello, ${name}\nAnwser "yes" if number is even, otherwise answer "no".`);
+let num = Math.floor(Math.random() * 100);
+const question = readlineSync.question('Question: ' + num);
+readlineSync.setDefaultOptions({limit: ['yes', 'no']});
+const answer = readlineSync.question('Your answer: ');
+const isEven = (result) => {
+  if (num % 2 === 0 && answer === 'yes') {
+    return 'Correct!';
+  } else {
+    return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
+  };
+};
+const answer2 = readlineSync.question('Your answer: ');
+const isEven2 = (result) => {
+  if (num % 2 === 0 && answer2 === 'yes') {
+    return 'Correct!';
+  } else {
+    return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
+  };
+};
+const answer3 = readlineSync.question('Your answer: ');
+const isEven3 = (result) => {
+  if (num % 2 === 0 && answer3 === 'yes') {
+    return 'Correct!';
+  } else {
+    return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
+  };
+};
+
 
 console.log(`Congratulations, ${name}!`);
 
