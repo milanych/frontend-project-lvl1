ecdc8fecbe0389d015f4004873943f7f58f3391f
milanych <vadimzotov1988@gmail.com>
2021-03-11 22:15:14 +0300

    Update brian-even.js

--- bin/brian-even.js
+++ bin/brian-even.js
@@ -2,9 +2,6 @@
 import readlineSync from 'readline-sync';
 const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ')
 console.log(`Hello, ${name}\nAnwser "yes" if number is even, otherwise answer "no".`);
-let num = Math.floor(Math.random() * 100);
-console.log('Question: ' + num);
-const answer = readlineSync.question('Your answer: ');
 const random = () => {
   let result = [];
   for (let x = 0; x < 3; x++) {
@@ -15,7 +12,8 @@ const random = () => {
 let rand = random();
 const game = () => {
   for (let i of rand) {
-    if (i % 2 === 0 && answer === 'yes') {
+    console.log('Question: ' + i);
+    if (i % 2 === 0 && readlineSync.question('Your answer: ') === 'yes') {
       console.log('Correct!');
     } 
     console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`)
