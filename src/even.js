cc38c65099c3564703da2dfdfe992af317364ee6
milanych <vadimzotov1988@gmail.com>
2021-03-11 21:54:44 +0300

    Update even.js

--- src/even.js
+++ src/even.js
@@ -13,17 +13,17 @@ const isEven = (result) => {
     return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
   };
 };
-const answer = readlineSync.question('Your answer: ');
-const isEven = (result) => {
-  if (num % 2 === 0 && answer === 'yes') {
+const answer2 = readlineSync.question('Your answer: ');
+const isEven2 = (result) => {
+  if (num % 2 === 0 && answer2 === 'yes') {
     return 'Correct!';
   } else {
     return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
   };
 };
-const answer = readlineSync.question('Your answer: ');
-const isEven = (result) => {
-  if (num % 2 === 0 && answer === 'yes') {
+const answer3 = readlineSync.question('Your answer: ');
+const isEven3 = (result) => {
+  if (num % 2 === 0 && answer3 === 'yes') {
     return 'Correct!';
   } else {
     return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`
