536f482abb5355a52aefc7600a37d6a9b843ff6b
milanych <vadimzotov1988@gmail.com>
2021-03-15 23:03:37 +0300

    added summator

--- games/calc.js
+++ games/calc.js
@@ -4,6 +4,7 @@ import { name, random6 } from '../src/index.js';
 export const game = () => {
   const rand = random6();
   let max = rand.length - 1;
+  let summator = 0;
   const signs = ['+', '-', '*'];
   const result = [];
   for (let i = 0; i < rand.length / 2; i += 1, max -= 1) {
@@ -22,10 +23,13 @@ export const game = () => {
     const answer = readlineSync.question('Your answer: ');
     if (result[i] === Number(answer)) {
       console.log('Correct!');
+      summator += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result[i]}'.\nLet's try again, ${name}!`);
       break;
     }
   }
-  console.log(`Congratulations, ${name}!`);
+  if (summator === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
 };
--- games/even.js
+++ games/even.js
@@ -3,6 +3,7 @@ import { random3, name } from '../src/index.js';
 
 console.log(`Hello, ${name}\nAnwser "yes" if number is even, otherwise answer "no".`);
 export const game = () => {
+  let summator = 0;
   const rand = random3();
   for (let i = 0; i < rand.length;) {
     const isEven = () => {
@@ -16,10 +17,13 @@ export const game = () => {
     if (answer === isEven()) {
       console.log('Correct!');
       i += 1;
+      summator += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven()}'.\nLet's try again, ${name}!`);
       break;
     }
+  }
+  if (summator === 3) {
     console.log(`Congratulations, ${name}!`);
   }
 };
