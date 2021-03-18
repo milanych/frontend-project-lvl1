0793befa41157187ee1f3754a852b8b829472ac3
milanych <vadimzotov1988@gmail.com>
2021-03-18 18:45:21 +0300

    fixed cli.js again

--- src/cli.js
+++
@@ -1,6 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
- GNU nano 4.8                                                     src/cli.js                                                               import readlineSync from 'readline-sync';
-
-export const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
