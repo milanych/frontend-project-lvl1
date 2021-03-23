12202e54b07ed797b412e64815cfd7cf0072dcc1
milanych <vadimzotov1988@gmail.com>
2021-03-23 21:11:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -58,3 +58,104 @@ export const even = () => {
     console.log(`Congratulations, ${name}!`);
   }
 };
+
+export const divisor = () => {
+  const result = [];
+  const sum = [];
+  let summator = 0;
+  for (let z = 0; z < 3;) {
+    const arr = random2();
+    const sorted = arr.sort();
+    for (let i = 0; i <= sorted[0]; i += 1) {
+      const cur1 = sorted[0] / i;
+      const round1 = Math.round(sorted[0] / i);
+      const cur2 = sorted[1] / i;
+      const round2 = Math.round(sorted[1] / i);
+      if (cur1 === round1 && cur2 === round2) {
+        result.push(i);
+      }
+    }
+    sum.push(result[result.length - 1]);
+    console.log(`Question: ${sorted[0]} ${sorted[1]}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (sum[z] === Number(answer)) {
+      console.log('Correct!');
+      z += 1;
+      summator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${sum[z]}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  if (summator === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export const prime = () => {
+  for (let z = 0; z < 3;) {
+    let result = 0;
+    let question = '';
+    const random = Math.floor(Math.random() * 100);
+    if (random > 1) {
+      for (let i = 2; i < random; i += 1) {
+        const num = random % i;
+        if (num === 0) {
+          result = 1;
+          break;
+        }
+      }
+    } else {
+      result = 1;
+    }
+    if (result === 1) {
+      question = 'no';
+    } else {
+      question = 'yes';
+    }
+    console.log(`Question: ${random}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (question === answer) {
+      console.log('Correct!');
+      z += 1;
+      summator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${question}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  if (summator === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export const progression = () => {
+  for (let z = 0; z < 3;) {
+    const result = [];
+    const prog = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const len = [5, 6, 7, 8, 9, 10];
+    const lenRandom = len[Math.floor(Math.random() * 6)];
+    const random = prog[Math.floor(Math.random() * 10)];
+
+    let start = Math.floor(Math.random() * 10);
+    for (let i = 0; i < lenRandom; i += 1) {
+      result.push(start);
+      start += random;
+    }
+    result.splice(random, 1, '...');
+    const str = result.join(' ');
+    console.log(`Question: ${str}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((result[result.indexOf('...') - 1] + random) === Number(answer)) {
+      console.log('Correct!');
+      z += 1;
+      summator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result[result.indexOf('...') - 1] + random}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  if (summator === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
