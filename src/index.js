8c5777f228c33558d3a02b9752eb81389705f2cb
milanych <vadimzotov1988@gmail.com>
2021-03-23 21:05:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,3 +32,29 @@ export const random = () => {
   const progressor = Math.floor(Math.random() + 10);
   return prog[progressor];
 };
+
+export const even = () => {
+  let summator = 0;
+  const rand = random3();
+  for (let i = 0; i < rand.length;) {
+    const isEven = () => {
+      if (rand[i] % 2 === 0) {
+        return 'yes';
+      }
+      return 'no';
+    };
+    console.log(`Question: ${rand[i]}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === isEven()) {
+      console.log('Correct!');
+      i += 1;
+      summator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven()}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  if (summator === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
