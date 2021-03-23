adc8f0b642d67e6ddbbc82a8c4c9a7232831007d
milanych <vadimzotov1988@gmail.com>
2021-03-23 21:41:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -135,23 +135,23 @@ export const progression = () => {
     const prog = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const len = [5, 6, 7, 8, 9, 10];
     const lenRandom = len[Math.floor(Math.random() * 6)];
-    const random = prog[Math.floor(Math.random() * 10)];
+    const randomProg = prog[Math.floor(Math.random() * 10)];
 
     let start = Math.floor(Math.random() * 10);
     for (let i = 0; i < lenRandom; i += 1) {
       result.push(start);
-      start += random;
+      start += randomProg;
     }
-    result.splice(random, 1, '..');
+    result.splice(randomProg, 1, '..');
     const str = result.join(' ');
     console.log(`Question: ${str}`);
     const answer = readlineSync.question('Your answer: ');
-    if ((result[result.indexOf('..') - 1] + random) === Number(answer)) {
+    if ((result[result.indexOf('..') - 1] + randomProg) === Number(answer)) {
       console.log('Correct!');
       z += 1;
       summator += 1;
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result[result.indexOf('..') - 1] + random}'.\nLet's try again, ${name}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result[result.indexOf('..') - 1] + randomProg}'.\nLet's try again, ${name}!`);
       break;
     }
   }
