d4a76682acded4d30edef472cd234e9a07fa67e1
milanych <vadimzotov1988@gmail.com>
2021-03-23 21:10:44 +0300

    Update progression.js

--- games/progression.js
+++ games/progression.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
 import { name, progression } from '../src/index.js';
-console.log('What number is missing in the progression?');
+console.log(`Hello, ${name}!\nWhat number is missing in the progression?`);
 progression();
