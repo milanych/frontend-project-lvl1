f9e5e8c678c90371f1e163215d70b69a916fbec2
milanych <vadimzotov1988@gmail.com>
2021-03-23 21:05:25 +0300

    Update even.js

--- games/even.js
+++ games/even.js
@@ -1,29 +1,5 @@
 import readlineSync from 'readline-sync';
-import { random3, name } from '../src/index.js';
+import { random3, name, even } from '../src/index.js';
 
 console.log(`Hello, ${name}\nAnwser "yes" if number is even, otherwise answer "no".`);
-export const game = () => {
-  let summator = 0;
-  const rand = random3();
-  for (let i = 0; i < rand.length;) {
-    const isEven = () => {
-      if (rand[i] % 2 === 0) {
-        return 'yes';
-      }
-      return 'no';
-    };
-    console.log(`Question: ${rand[i]}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === isEven()) {
-      console.log('Correct!');
-      i += 1;
-      summator += 1;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven()}'.\nLet's try again, ${name}!`);
-      break;
-    }
-  }
-  if (summator === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
-};
+even();
