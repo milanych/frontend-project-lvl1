97f3cc271735a411953c5c3658d7c1cee324d0bc
milanych <vadimzotov1988@gmail.com>
2021-03-28 10:09:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,8 +42,52 @@ export const random = () => {
   const progressor = Math.floor(Math.random() + 10);
   return prog[progressor];
 };
-export const sum = () => {
-    if (summator === 3) {
+
+const game = (logic) => {
+  let summator = 0;
+  if (logic === calc) {
+  const rand = random6();
+  let max = rand.length - 1;
+  const signs = ['+', '-', '*'];
+  const result = [];
+  
+  for (let i = 0; i < rand.length / 2; i += 1, max -= 1) {
+    const randSign = signs[Math.floor(Math.random() * 3)];
+    result.push(mathResult(rand[i], randSign, rand[max]));
+    console.log(`Question: ${rand[i]} ${randSign} ${rand[max]}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (result[i] === Number(answer)) {
+      console.log('Correct!');
+      summator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result[i]}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  } else if (logic === even) {
+    const rand = random3();
+
+  for (let i = 0; i < rand.length;) {
+    console.log(`Question: ${rand[i]}`);
+    const answer = readlineSync.question('Your answer: ');
+    const isEven = () => {
+      if (rand[i] % 2 === 0) {
+        return 'yes';
+      }
+      return 'no';
+    };
+    if (answer === isEven()) {
+      console.log('Correct!');
+      i += 1;
+      summator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven()}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  }
+  if (summator === 3) {
     console.log(`Congratulations, ${name}!`);
   }
-}
+};
+export default game;
