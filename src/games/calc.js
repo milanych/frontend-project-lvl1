7c89fc09c7d0506f6e06afbd56d8516d33f1a1f9
milanych <vadimzotov1988@gmail.com>
2021-04-18 19:16:40 +0300

    fixed

--- src/games/calc.js
+++ src/games/calc.js
@@ -18,9 +18,9 @@ const gameData = () => {
   const randSign = signs[Math.floor(Math.random() * 3)];
   const number1 = generateRandomNumber();
   const number2 = generateRandomNumber();
-  const r = mathResult(number1, randSign, number2);
-  const q = `${number1} ${randSign} ${number2}`;
-  return [r, q];
+  const result = mathResult(number1, randSign, number2);
+  const question = `${number1} ${randSign} ${number2}`;
+  return [result, question];
 };
 const enter = console.log("Find the greatest common divisor of given numbers.");
 export default () => startGame(enter, gameData);
