20cffae6a1482da850c7c27a2911e38376ea1a7b
milanych <vadimzotov1988@gmail.com>
2021-04-18 19:21:53 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import readlineSync from "readline-sync";
 const rounds = 3;
 
 export default (enter, startGame) => {
-  const name = readlineSync.question(
-    "Welcome to the Brain Games!\nMay I have your name? "
-  );
-  console.log(`Hello, ${name}!`);
+  const name = greet();
   for (let summator = 0; summator < rounds; summator += 1) {
     const [correctAnswer, question] = startGame();
     console.log(`Question: ${question}`);
