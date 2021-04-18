bae023d538b15522ab11fba557b0cea148bea261
milanych <vadimzotov1988@gmail.com>
2021-04-18 18:39:54 +0300

    fixed

--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,19 @@
-import generateRandomNumber from "../util.js";
-import startGame from "../index.js";
+import generateRandomNumber from '../util.js';
+import startGame from '../index.js';
 
 const gameData = () => {
   const question = generateRandomNumber();
   const isEven = () => {
     if (question % 2 === 0) {
-      return "yes";
+      return 'yes';
     }
-    return "no";
+    return 'no';
   };
   const result = String(isEven());
   return [result, question];
 };
 
 const enter = console.log(
-  'Answer "yes" if number is even, otherwise answer "no".'
+  'Answer "yes" if number is even, otherwise answer "no".',
 );
 export default () => startGame(enter, gameData);
