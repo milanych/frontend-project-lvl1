8ddbece5f23b9abf2abca06b788a246506cd4432
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-04-23 21:45:44 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import readlineSync from 'readline-sync';
-import greet from './cli.js';
 
 const rounds = 3;
 
 export default (enter, startGame) => {
-  const name = greet();
+  const greet = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    return name;
+  };
   for (let summator = 0; summator < rounds; summator += 1) {
     const [correctAnswer, question] = startGame();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (String(correctAnswer) !== String(answer)) {
       console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
       );
-      console.log(`Let's try again, ${name}!`);
+      console.log(`Let's try again, ${greet()}!`);
       break;
     }
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${greet()}!`);
 };
