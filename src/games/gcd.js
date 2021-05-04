bb95afb39bb91d03e40288aa989df62215c4e077
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-04 22:47:54 +0300

    fixed gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,26 +1,26 @@
 import generateRandomNumber from '../util.js';
 import startGame from '../index.js';
 
-const description = 'Find the greatest common divisor of given numbers.';
+const gameDescription = 'Find the greatest common divisor of given numbers.';
 const isGcd = (num1, num2) => {
   let result = 0;
   for (let i = 0; i <= num1; i += 1) {
-    const cur1 = num1 / i;
-    const round1 = Math.round(num1 / i);
-    const cur2 = num2 / i;
-    const round2 = Math.round(num2 / i);
-    if (cur1 === round1 && cur2 === round2) {
+    const currentNum1 = num1 / i;
+    const roundedCurrentNum1 = Math.round(num1 / i);
+    const currentNum2 = num2 / i;
+    const roundedCurrentNum2 = Math.round(num2 / i);
+    if (currentNum1 === roundedCurrentNum1 && currentNum2 === roundedCurrentNum2) {
       result = i;
     }
   }
   return result;
 };
 
-const gameData = () => {
+const getGameData = () => {
   const randomNumbers = [generateRandomNumber(1, 100), generateRandomNumber(1, 100)];
-  const sorted = randomNumbers.sort();
-  const answer = String(isGcd(sorted[0], sorted[1]));
-  const question = `${sorted[0]} ${sorted[1]}`;
+  const sortedRandomNumbers = randomNumbers.sort();
+  const answer = String(isGcd(sortedRandomNumbers[0], sortedRandomNumbers[1]));
+  const question = `${sortedRandomNumbers[0]} ${sortedRandomNumbers[1]}`;
   return [answer, question];
 };
-export default () => startGame(description, gameData);
+export default () => startGame(gameDescription, getGameData);
