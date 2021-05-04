d802f60f65a83dadb221e4f2d8198a021a824ba0
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-04 12:26:11 +0300

    fixed prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,28 +3,23 @@ import startGame from '../index.js';
 
 const description = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 const isPrime = (randomNumber) => {
-  let result = 0;
+  let result = false;
   if (randomNumber > 1) {
     for (let i = 2; i < randomNumber; i += 1) {
       const num = randomNumber % i;
       if (num === 0) {
-        result = 1;
+        result = true;
         break;
       }
     }
   } else {
-    result = 1;
+    result = true;
   }
   return result;
 };
 const gameData = () => {
   const randomPrime = generateRandomNumber();
-  let question = '';
-  if (isPrime(randomPrime) === 1) {
-    question = 'no';
-  } else {
-    question = 'yes';
-  }
+  const question = isPrime(randomPrime) ? 'no' : 'yes';
   return [question, randomPrime];
 };
 export default () => startGame(description, gameData);
