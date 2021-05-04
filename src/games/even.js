6593828791780114f79f72337a6293b05b02e05f
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-04 10:22:59 +0300

    fixed

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import startGame from '../index.js';
 import isEven from '../boolean.js';
 
 const description = 'Answer "yes" if number is even, otherwise answer "no".';
-const gameData = () => {
+const gameData = (question, answer) => {
   const [question, answer] = isEven();
   return [question, answer];
 };
