a0b2e98f3935bd8ffeeca4dd37c49f048e955563
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-04 22:44:49 +0300

    fixed gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,15 +2,13 @@ import generateRandomNumber from '../util.js';
 import startGame from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
-const randomNumbers = [generateRandomNumber(1, 100), generateRandomNumber(1, 100)];
-const sorted = randomNumbers.sort();
-const isGcd = () => {
+const isGcd = (num1, num2) => {
   let result = 0;
-  for (let i = 0; i <= sorted[0]; i += 1) {
-    const cur1 = sorted[0] / i;
-    const round1 = Math.round(sorted[0] / i);
-    const cur2 = sorted[1] / i;
-    const round2 = Math.round(sorted[1] / i);
+  for (let i = 0; i <= num1; i += 1) {
+    const cur1 = num1 / i;
+    const round1 = Math.round(num1 / i);
+    const cur2 = num2 / i;
+    const round2 = Math.round(num2 / i);
     if (cur1 === round1 && cur2 === round2) {
       result = i;
     }
@@ -19,7 +17,9 @@ const isGcd = () => {
 };
 
 const gameData = () => {
-  const answer = String(isGcd());
+  const randomNumbers = [generateRandomNumber(1, 100), generateRandomNumber(1, 100)];
+  const sorted = randomNumbers.sort();
+  const answer = String(isGcd(sorted[0], sorted[1]));
   const question = `${sorted[0]} ${sorted[1]}`;
   return [answer, question];
 };
