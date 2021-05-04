7fe8cc3a1c8b054672a481f1ce15d20c5def7a42
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-04 10:26:23 +0300

    fixed

--- src/boolean.js
+++ src/boolean.js
@@ -2,8 +2,10 @@ import generateRandomNumber from './util.js';
 
 export default () => {
   const randomNumber = generateRandomNumber();
+  const yes = 'yes';
+  const no = 'no';
   if (randomNumber % 2 === 0) {
-    return [randomNumber, 'yes'];
+    return [randomNumber, yes];
   }
-  return [randomNumber, 'no'];
+  return [randomNumber, no];
 };
--- src/games/even.js
+++ src/games/even.js
@@ -4,9 +4,7 @@ import isEven from '../boolean.js';
 const description = 'Answer "yes" if number is even, otherwise answer "no".';
 const gameData = () => {
   const [question, answer] = isEven();
-  const que = question;
-  const ans = answer;
-  return [que, ans];
+  return [question, answer];
 };
 
 export default () => startGame(description, gameData);
