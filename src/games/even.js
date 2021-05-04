671e79f6e3e4ff105e229b3daeea8475eb073024
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-04 10:25:17 +0300

    fixed

--- src/games/even.js
+++ src/games/even.js
@@ -2,9 +2,11 @@ import startGame from '../index.js';
 import isEven from '../boolean.js';
 
 const description = 'Answer "yes" if number is even, otherwise answer "no".';
-const gameData = (question, answer) => {
+const gameData = () => {
   const [question, answer] = isEven();
-  return [question, answer];
+  const que = question;
+  const ans = answer;
+  return [que, ans];
 };
 
 export default () => startGame(description, gameData);
