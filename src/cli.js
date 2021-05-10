a441980041c41f1f2837f732512fc674a9c931f2
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-10 11:05:09 +0300

    fixed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+const greet = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+export default greet;
