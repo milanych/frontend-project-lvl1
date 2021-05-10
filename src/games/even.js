1688161d5acfdd8c53ce686a96b37979fb04be7c
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-10 10:54:08 +0300

    deleted boolean.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,15 @@
 import startGame from '../index.js';
-import isEven from '../boolean.js';
+import generateRandomNumber from '../util.js';
 
+const isEven = () => {
+  const randomNumber = generateRandomNumber();
+  const yes = 'yes';
+  const no = 'no';
+  if (randomNumber % 2 === 0) {
+    return [yes, randomNumber];
+  }
+  return [no, randomNumber];
+};
 const gameDescription = 'Answer "yes" if number is even, otherwise answer "no".';
 const getGameData = () => {
   const [question, answer] = isEven();
