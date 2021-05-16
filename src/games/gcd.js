828b4c3c8758212136a0b85f1d04a5bbd2c04eb0
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-16 17:06:14 +0300

    fixed gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,18 +2,13 @@ import generateRandomNumber from '../util.js';
 import startGame from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
-const getGcd = (num1, num2) => {
-  if (num2 % num1 === 0) {
-    return num1;
-  }
-  return getGcd(num2, num1 % num2);
-};
+const getGcd = (num1, num2) => (num2 % num1 === 0 ? num1 : getGcd(num2, num1 % num2));
 
 const getRound = () => {
   const number1 = generateRandomNumber(1, 100);
   const number2 = generateRandomNumber(1, 100);
   const answer = getGcd(number1, number2);
-  const question = `${number1} and ${number2}`;
+  const question = `${number1} ${number2}`;
   return [String(answer), String(question)];
 };
 export default () => startGame(description, getRound);
