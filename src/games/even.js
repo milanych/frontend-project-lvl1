51672c00eccc11da1f62f59406ac3e95cf8f6a24
Milanych <vadim_system@DESKTOP-T41HDBU.localdomain>
2021-05-27 20:47:16 +0300

    fixed calc game

--- src/games/even.js
+++ src/games/even.js
@@ -4,8 +4,8 @@ import generateRandomNumber from '../util.js';
 const description = 'Answer "yes" if number is even, otherwise answer "no".';
 const isEven = (number) => (number % 2 === 0);
 const getRound = () => {
-  const answer = isEven(generateRandomNumber()) ? 'yes' : 'no';
   const question = generateRandomNumber();
+  const answer = isEven(question) ? 'yes' : 'no';
   return [answer, question];
 };
 
